227aca8d21c09edef1282e53686e4996dc709397
EricPaulino <paulino.eric@escolar.ifrn.edu.br>
2023-10-25 11:19:40 -0300

    partes do script feito, falta melhorar a animação

--- jogo do renan/Assets/Boss 3 - Eric Paulino/Scripts/BossEric.cs
+++ jogo do renan/Assets/Boss 3 - Eric Paulino/Scripts/BossEric.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class BossEric : MonoBehaviour
 {
+    public float Speed;
+    public Rigidbody2D rig;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    private void FixedUpdate()
+    {
+    
+    
     }
 }
