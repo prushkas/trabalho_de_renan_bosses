27d62c7d5e1acfc899672cdc27ef345f547987d8
prushkas <cefasnatanael@gmail.com>
2023-10-27 10:12:14 -0300

    falta arrumar a espada e o dash

--- jogo do renan/Assets/player/scripts/Player.cs
+++ jogo do renan/Assets/player/scripts/Player.cs
@@ -6,21 +6,35 @@ public class Player : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
+    private bool isJump;
+    private bool doubleJump;
+    public float timeToExitAttack;
+    private bool isFire;
+    private bool isAttacking;
+
+    public GameObject fireBall;
+    public GameObject sword;
+    public Transform firePoint;
+    public Transform swordPoint;
+
 
     private Rigidbody2D rig;
     private Animator anim;
+
+    private float movement;
     
-    // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
+        Jump();
+        SoulFire();
+        Espadada();
     }
     
     void Move()
@@ -31,20 +45,115 @@ public class Player : MonoBehaviour
 
         if (movement > 0)
         {
+            anim.SetBool("isrun", true);
             transform.eulerAngles = new Vector3(0,0,0);
         }
 
         if (movement < 0)
         {
+            anim.SetBool("isrun", true);
             transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        if (movement == 0)
+        {
+            anim.SetBool("isrun", false);
         }
     }
 
     void Jump()
     {
-        if (Input.GetButtonDown("Space"))
+        if (Input.GetButtonDown("Jump"))
         {
-            rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            if (!isJump)
+            {
+                anim.SetBool("isjump", true);
+                rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+                isJump = true;
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0, jumpForce * 2), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+        }
+    }
+
+    void SoulFire()
+    {
+        StartCoroutine("Fire");
+    }
+
+    IEnumerator Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (isFire == false)
+            {
+                isFire = true;
+                anim.SetBool("isfiring", true);
+                GameObject bolaFogo = Instantiate(fireBall, firePoint.position, firePoint.rotation);
+                Invoke(nameof(exitAttack), timeToExitAttack);
+
+                if (transform.rotation.y == 0)
+                {
+                    bolaFogo.GetComponent<BolaDeAlmaDeFogo>().isRight = true;
+                }
+                if(transform.rotation.x == 180)
+                {
+                    bolaFogo.GetComponent<BolaDeAlmaDeFogo>().isRight = false;
+                }
+
+                yield return new WaitForSeconds(0.3f);
+                anim.SetBool("isfiring", false);
+            }
+        }
+    }
+
+    void Espadada()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (isAttacking == false)
+            {
+                isAttacking = true;
+                anim.SetBool("isattacking", true);
+                Instantiate(sword, swordPoint.position, swordPoint.rotation);
+                Invoke(nameof(exitAttack), timeToExitAttack);
+
+
+                if (movement > 0)
+                {
+                    sword.transform.eulerAngles = new Vector3(0, 0, 0);
+                }
+
+                if (movement < 0)
+                {
+                    sword.transform.eulerAngles = new Vector3(0, 180, 0);
+                }
+
+                isAttacking = false;
+                anim.SetBool("isattacking", false);
+            }
+        }
+    }
+
+
+    void exitAttack()
+    {
+        isFire = false;
+        anim.SetBool("isfiring", false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            anim.SetBool("isjump", false);
+            isJump = false;
         }
     }
 }
